183a0f5b9a8ee886e41d358d012be999cb7fdad5
Weston Littrell <westonlittrell@gmail.com>
2018-04-30 17:15:25 -0500

    fix maxbuffer issue

--- index.js
+++ index.js
@@ -1,72 +1,65 @@
 #!/usr/bin/env node
-'use strict';
+const path = require('path')
+const spawnAsync = require('@expo/spawn-async')
+const epubcheck_path = path.resolve(__dirname, 'lib/epubcheck')
 
-const path = require('path');
-const remove = require('lodash.remove');
-const execAsync = require('async-child-process').execAsync;
-const shellEscape = require('shell-escape');
-const epubcheck_path = path.resolve(__dirname, 'lib/epubcheck');
-
-
-async function epubCheck(dir_path) {
-  const args = ['java', '-jar', `${epubcheck_path}/epubcheck.jar`, dir_path, '--quiet', '--mode', 'exp', '-c', `${epubcheck_path}/custom-msg.txt`];
-  const command = shellEscape(args);
-  let result = await processExecData(command);
-  return result;
-
-  async function processExecData(command) {
-    let output = await execAsync(command)
-      .then(res => {return res;}).catch(err => {return err;});
-    output = output.toString();
-    let lines = output.split("\n");
-
-    if (lines.length > 1) {
-      lines = lines.map(line => {
-        // Replace path in logs to make it cleaner
-        line = line.replace(new RegExp(`${dir_path}.epub/`, 'g'), '');
-
-        // Split err messages for object
-        const parts = line.split(': ');
-        const type = parts[0];
-        const loc = parts[1];
-        const loc_regex = /^(.*?)\(([^\,]+),([^\)]+)\)/;
-        const loc_arr = loc_regex.exec(loc);
-        let file = '',
-            line_num = '',
-            col = '';
-
-        if (loc_arr) {
-          file = loc_arr[1];
-          line_num = loc_arr[2];
-          col = loc_arr[3];
-        }
-
-        const msg = parts[2];
-
-        return {
-          'type': type,
-          'file': file,
-          'line': line_num,
-          'col': col,
-          'msg': msg
-        };
-      });
-
-      remove(lines, l => {
-        return !l.file;
-      });
+function linesToMessages(dir_path, lines) {
+  return lines
+    .map(line => {
+      // Replace path in logs to make it cleaner
+      // Split err messages for object
+      const parts = line
+        .replace(new RegExp(`${dir_path}.epub/`, 'g'), '')
+        .split(': ')
+      const loc = parts[1]
+      const loc_regex = /^(.*?)\(([^,]+),([^)]+)\)/
+      const loc_arr = loc_regex.exec(loc)
 
       return {
-        pass: false,
-        messages: lines
-      };
-    } else {
-      return {
-        pass: true,
-        messages: []
-      };
-    }
-  }
+        type: parts[0],
+        file: loc_arr ? loc_arr[1] : '',
+        line: loc_arr ? loc_arr[2] : '',
+        col: loc_arr ? loc_arr[3] : '',
+        msg: parts[2],
+      }
+    })
+    .filter(l => !!l.file) // filter noise (errors always have a file property)
 }
 
-module.exports = epubCheck;
+async function processExecData(dir_path, command, args) {
+  const output = await spawnAsync(command, args)
+    .then(({ stdout }) => {
+      return stdout
+    })
+    .catch(({ stderr }) => {
+      return stderr
+    })
+
+  const lines = output.toString().split('\n')
+
+  return lines.length > 1
+    ? {
+        pass: false,
+        messages: linesToMessages(dir_path, lines),
+      }
+    : {
+        pass: true,
+        messages: [],
+      }
+}
+
+function epubCheck(dir_path) {
+  const args = [
+    '-jar',
+    `${epubcheck_path}/epubcheck.jar`,
+    dir_path,
+    '--quiet',
+    '--mode',
+    'exp',
+    '-c',
+    `${epubcheck_path}/custom-msg.txt`,
+  ]
+  return processExecData(dir_path, 'java', args)
+}
+
+module.exports = epubCheck
